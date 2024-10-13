d143e75ff975b6aa83d90bdece55bcfdcb0cb905
Florian Quirin <info@bytemind.de>
2024-10-13 21:11:29 +0200

    added some run and download scripts

---
+++ models/download_tinyllama-v1.1B-chat_gguf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -o tinyllama-1.1b-chat-v1.0.Q6_K.gguf -C - https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q6_K.gguf?download=true
