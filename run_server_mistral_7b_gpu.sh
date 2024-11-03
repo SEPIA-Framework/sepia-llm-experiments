3a064daa2f6a384c688320024cd8f17a0cd5dc1d
Florian Quirin <info@bytemind.de>
2024-11-03 16:52:50 +0100

    Update run_server_mistral_7b_gpu.sh

--- run_server_mistral_7b_gpu.sh
+++ run_server_mistral_7b_gpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd llama.cpp
-./llama-server -m ../models/Mistral-7B-Instruct-v0.3-Q4_K_M.gguf -t 4 --n-gpu-layers 33 --ctx-size 16384 --n-predict 512 --system-prompt-file ../prompts/SEPIA_smart_home_test_example.txt --keep -1 --port 20733
+./llama-server -m ../models/Mistral-7B-Instruct-v0.3-Q4_K_M.gguf -t 4 --n-gpu-layers 33 --ctx-size 16384 --n-predict 512 --slots --parallel 1 --port 20733
